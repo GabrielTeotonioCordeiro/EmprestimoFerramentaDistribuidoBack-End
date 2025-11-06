7c582ccfc96ae4da59167cfd24d7773024ce80dd
VictorLScheuerRodrigues <victor.scheuer.rodrigues@gmail.com>
2025-11-06 20:09:35 -0300

    refactor:Colocando tudo de ferramenta service dentro de ferramenta

--- src/main/java/modelo/Ferramenta.java
+++ src/main/java/modelo/Ferramenta.java
@@ -1,8 +1,9 @@
 package modelo;
 
-/**
- * Classe de ferramenta com nome, marca, custo e disponibilidade.
- */
+import dao.EmprestimoDAO;
+import dao.FerramentaDAO;
+import java.util.List;
+
 public class Ferramenta {
 
     private int idFerramenta;
@@ -45,6 +46,17 @@ public class Ferramenta {
     public String getNomeFerramenta() {
         return nomeFerramenta;
     }
+    
+    public static String buscarNomePorId(int id) {
+    FerramentaDAO dao = new FerramentaDAO();
+    List<Ferramenta> listaFerramenta = dao.getListaFerramenta();
+    for (Ferramenta f : listaFerramenta) {
+        if (f.getIdFerramenta() == id) {
+            return f.getNomeFerramenta();
+        }
+    }
+    return "";
+}
 
     public void setNomeFerramenta(String nome) {
         this.nomeFerramenta = nome;
@@ -61,6 +73,96 @@ public class Ferramenta {
 
     public void setCustoFerramenta(double custo) {
         this.custoFerramenta = custo;
+    }
+
+    /**
+     * metodos de banco de dados
+     */
+    public boolean salvar() {
+        FerramentaDAO dao = new FerramentaDAO();
+        return dao.insertFerramentaDB(this);
+    }
+
+    public boolean atualizar() {
+        FerramentaDAO dao = new FerramentaDAO();
+        return dao.updateFerramentaDB(this);
+    }
+
+    public boolean deletar() {
+        FerramentaDAO dao = new FerramentaDAO();
+        return dao.deleteFerramentaDB(this.idFerramenta);
+    }
+
+// Método estático, porque não depende de uma instância
+    public static Ferramenta buscarPorId(int id) {
+        FerramentaDAO dao = new FerramentaDAO();
+        return dao.retrieveFerramentaDB(id);
+    }
+
+// Também pode ser estático
+    public static List<Ferramenta> listarTodas() {
+        FerramentaDAO dao = new FerramentaDAO();
+        return dao.getListaFerramenta();
+    }
+
+// E para gerar um novo ID quando criar uma ferramenta nova
+    public static int proximoID() {
+        FerramentaDAO dao = new FerramentaDAO();
+        return dao.maiorIDFerramenta() + 1;
+    }
+
+    public boolean insertFerramentaDB(String nome, String marca, double custo) {
+        Ferramenta ferramenta = new Ferramenta(Ferramenta.proximoID(), nome, custo, marca);
+        return ferramenta.salvar();
+    }
+
+    public boolean updateFerramentaDB(int id, String nome, String marca, double custo) {
+        Ferramenta ferramenta = new Ferramenta(id, nome, custo, marca);
+        return ferramenta.atualizar();
+    }
+
+    public boolean deleteFerramentaDB(int id) {
+        Ferramenta ferramenta = new Ferramenta();
+        ferramenta.setIdFerramenta(id);
+        return ferramenta.deletar();
+    }
+
+    public String getDisponivel() {
+        EmprestimoDAO empDAO = new EmprestimoDAO();
+        List<Emprestimo> emprestimosAtivos = empDAO.getListaEmprestimo();
+
+        for (Emprestimo e : emprestimosAtivos) {
+            if (e.getIDFerramenta() == this.idFerramenta) {
+                return "Não";
+            }
+        }
+        return "Sim";
+    }
+
+    public boolean possuiEmprestimoAtivo() {
+        EmprestimoDAO empDAO = new EmprestimoDAO();
+        List<Emprestimo> emprestimosAtivos = empDAO.getListaEmprestimo();
+
+        for (Emprestimo e : emprestimosAtivos) {
+            if (e.getIDFerramenta() == this.idFerramenta) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int quantidadeEmprestimo() {
+        EmprestimoDAO empDAO = new EmprestimoDAO();
+        List<Emprestimo> emprestimos = empDAO.getListaEmprestimo();
+
+        int contador = 0;
+        for (Emprestimo e : emprestimos) {
+            if (e.getIDFerramenta() == this.idFerramenta) {
+                contador++;
+            }
+
+        }
+        return contador;
     }
 
     /**
